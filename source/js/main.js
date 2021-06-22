ffa8055c0b0753af38cf9cea87c06ff87e5bf73b
Sergey <serezha.ros@mail.ru>
2021-06-22 06:37:24 +0300

    block tab

--- source/js/main.js
+++ source/js/main.js
@@ -2,71 +2,11 @@ window.addEventListener('load', function () {
 
     "use strict";
 
-    // let btnMenu = document.querySelector('.menu-btn');
 
-    // btnMenu.addEventListener('click', function() {
-    //     btnMenu.classList.toggle('menu-btn--open-close')
-    // });
+    // ---------- slider ------------    
 
 
-    function burgerMenu(selector) {
-        let menu = document.querySelector(selector);
-        let btnMenu = document.querySelector('.menu-btn');
-        let links = document.querySelectorAll('.menu__navigation-link');
-        let menuInner = document.querySelector('.menu__inner');
-
-        btnMenu.addEventListener('click', function (e) {
-            e.preventDefault();
-            toggleMenu();
-        });
-
-        // links.addEventListener('click', function () {
-        //     toggleMenu();
-        // });
-        links.forEach(item => {
-            item.addEventListener('click', function () {
-                toggleMenu()
-            })
-        });
-
-        // menuInner.addEventListener('click', function (e) {
-        //     if (e.target === menuInner) {
-        //         console.log(e.target);
-        //         toggleMenu();
-        //     }
-
-
-        // });
-        menu.addEventListener('click', function () {
-            // if (e.target === menu) {
-            //     console.log(e.target);
-            //     toggleMenu();
-            // }
-
-            toggleMenu();
-        });
-
-        function toggleMenu() {
-            menu.classList.toggle('menu--active');
-            btnMenu.classList.toggle('menu-btn--open-close');
-            if (menu.classList.contains('menu--active')) {
-                document.body.style.overflow = 'hidden';
-            } else {
-                document.body.style.overflow = 'visible';
-            }
-        }
-    }
-
-    burgerMenu('.menu');
-
-// ---------- slider ------------    
-
-    $('.about-us__slider').slick({
-        prevArrow: '<button class="about-us__slider-btn  about-us__slider-btnprev"><img src="images/slider-btn-left.svg" alt="стрелка влево"></button>',
-        nextArrow: '<button class="about-us__slider-btn  about-us__slider-btnnext"><img src="images/slider-btn-right.svg" alt="стрелка вправо"></button>',
-    });
-
-    $('.slider-tab').slick({ 
+    $('.slider-tab').slick({
         slidesToShow: 3,
         prevArrow: '<button class="slider-tab__slider-btn  slider-tab__slider-btnprev"><img src="images/slider-btn-left.svg" alt="стрелка влево"></button>',
         nextArrow: '<button class="slider-tab__slider-btn  slider-tab__slider-btnnext"><img src="images/slider-btn-right.svg" alt="стрелка вправо"></button>',
@@ -85,4 +25,32 @@ window.addEventListener('load', function () {
             },
         ],
     });
+
+
+
+    // ---------- tabs ------------   
+
+    const toSwitchTabs = (triggerBtn, content, activeBtn, activeContent) => {
+        const tabTrigger = document.querySelectorAll(triggerBtn);
+        const tabContent = document.querySelectorAll(content);
+
+        tabTrigger.forEach((item) => {
+            item.addEventListener('click', function (e) {
+                e.preventDefault();
+                const id = e.target.getAttribute('href').replace('#', '');
+
+                tabTrigger.forEach((child) => child.classList.remove(activeBtn));
+                tabContent.forEach((child) => child.classList.remove(activeContent));
+
+                item.classList.add(activeBtn);
+                document.getElementById(id).classList.add(activeContent);
+            });
+        });
+    };
+
+    toSwitchTabs('.tabs__triggers-item', '.tabs__content-item', 'tabs__triggers-item--active', 'tabs__content-item--active');
 });
+
+
+
+
